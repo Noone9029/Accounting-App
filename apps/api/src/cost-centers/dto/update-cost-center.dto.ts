0f7ec266cf2a7b8a61c2dba69b7e8929f8f87c6f
Muhammad Ahmad <ahmadkhalid9029@gmail.com>
2026-07-10 14:29:03 +0500

    fix(api): reject null dimension updates

--- apps/api/src/cost-centers/dto/update-cost-center.dto.ts
+++ apps/api/src/cost-centers/dto/update-cost-center.dto.ts
@@ -1,10 +1,10 @@
 import { PartialType } from "@nestjs/mapped-types";
 import { DimensionStatus } from "@prisma/client";
-import { IsEnum, IsOptional } from "class-validator";
+import { IsEnum, ValidateIf } from "class-validator";
 import { CreateCostCenterDto } from "./create-cost-center.dto";
 
-export class UpdateCostCenterDto extends PartialType(CreateCostCenterDto) {
-  @IsOptional()
+export class UpdateCostCenterDto extends PartialType(CreateCostCenterDto, { skipNullProperties: false }) {
+  @ValidateIf((_object, value) => value !== undefined)
   @IsEnum(DimensionStatus)
   status?: DimensionStatus;
 }
--- apps/api/src/projects/dto/update-project.dto.ts
+++ apps/api/src/projects/dto/update-project.dto.ts
@@ -1,10 +1,10 @@
 import { PartialType } from "@nestjs/mapped-types";
 import { DimensionStatus } from "@prisma/client";
-import { IsEnum, IsOptional } from "class-validator";
+import { IsEnum, ValidateIf } from "class-validator";
 import { CreateProjectDto } from "./create-project.dto";
 
-export class UpdateProjectDto extends PartialType(CreateProjectDto) {
-  @IsOptional()
+export class UpdateProjectDto extends PartialType(CreateProjectDto, { skipNullProperties: false }) {
+  @ValidateIf((_object, value) => value !== undefined)
   @IsEnum(DimensionStatus)
   status?: DimensionStatus;
 }
